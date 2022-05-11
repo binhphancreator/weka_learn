fe078640504458bc5a0f0e8586d3131cd3d41013
binhphancreator <tienkhach19@gmail.com>
2022-05-11 21:19:47 +0700

    add evaluate and predict

--- src/Model/KNNModel.java
+++ src/Model/KNNModel.java
@@ -1,6 +1,7 @@
 package Model;
 
 import weka.classifiers.lazy.IBk;
+import weka.core.Instances;
 import weka.classifiers.Evaluation;
 import java.util.Random;
 
@@ -13,11 +14,47 @@ public class KNNModel extends KnowLedgeModel {
   }
 
   public void buildKNN(String filename) {
+    try {
+      setDataSet(TYPE_DATASET.TRAIN, filename);
+      this.trainDataset.setClassIndex(this.trainDataset.numAttributes() - 1);
+      this.knn = new IBk();
+      //knn.setOptions(new String[]{"-A", "5"});
+      knn.buildClassifier(trainDataset);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
   }
   
-  public void evaluateKNN() {
+  public void evaluateKNN(String filename) {
+    setDataSet(TYPE_DATASET.TEST, filename);
+    this.testDataset.setClassIndex(this.testDataset.numAttributes() - 1);
+
     Random random = new Random();
     int folds = 10;
+
+    try {
+      evaluation = new Evaluation(this.testDataset);
+      evaluation.crossValidateModel(knn, this.testDataset, folds, random);
+      System.out.println(evaluation.toSummaryString());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void predictClassLable(String fileIn, String fileOut) {
+    Instances unlabel = loadDataSet(fileIn);
+    unlabel.setClassIndex(unlabel.numAttributes() - 1);
+
+    for (int i=0; i<unlabel.numAttributes() - 1 ; i++) {
+      double predict;
+      try {
+        predict = knn.classifyInstance(unlabel.instance(i));
+        unlabel.instance(i).setClassValue(predict);
+      } catch (Exception e) {
+        System.out.println("Error prediction");
+      }
+    }
+    System.out.println(unlabel.toString());
   }
 }
--- src/Model/KnowLedgeModel.java
+++ src/Model/KnowLedgeModel.java
@@ -4,34 +4,61 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class KnowLedgeModel {
-  protected DataSource source;
   protected Instances dataset;
-  public static enum TYPE_DATA_SET {
+  protected Instances trainDataset;
+  protected Instances testDataset;
+
+  public static enum TYPE_DATASET {
     TRAIN,
     TEST
   };
 
   public KnowLedgeModel(String filename) {
-    try  {
-      this.source = new DataSource(filename);
-      this.dataset = this.source.getDataSet();
+    this.dataset = loadDataSet(filename);
+  }
+
+  public DataSource loadDataSource(String sourceFile) {
+    try {
+      DataSource dataSource = new DataSource(sourceFile);
+      return dataSource;
     } catch (Exception e) {
-      System.out.println("Error reading dataset");
+      System.out.println("Error loading datasource");
+      return null;
     }
   }
 
-  public void setDataSet(TYPE_DATA_SET type, String filename) {
-    switch(type) {
-      case TRAIN:
-        break;
-      case TEST:
-        break;
-      default:
-        break;
+  public Instances loadDataSet(String sourceFile) {
+    try {
+      Instances instances = loadDataSource(sourceFile).getDataSet();
+      return instances;
+    } catch (Exception e) {
+      System.out.println("Error loading dataset");
+      return null;
     }
+  }
+
+  public boolean setDataSet(TYPE_DATASET type, String filename) {
+    try {
+      switch(type) {
+        case TRAIN:
+          this.trainDataset = loadDataSet(filename);
+          break;
+        case TEST:
+          this.testDataset = loadDataSet(filename);
+          break;
+        default:
+          break;
+      }
+      return true;
+    } catch(Exception e) {
+      System.out.println("Error loading dataset");
+      return false;
+    }
+    
   }
 
   public String toString() {
     return dataset.toSummaryString();
   }
+
 }
